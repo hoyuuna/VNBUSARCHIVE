fe229e2d91c4357bfce24a96f410064e507991d3
Hoyuuna <nghoanganhtuann@gmail.com>
2026-08-23 10:53:50 +0700

    Remove test files

--- test_sb.js
+++
@@ -1,2 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-const sb = createClient('https://qfblryrmewixxijmxhwv.supabase.co', 'YOUR_KEY_HERE'); // I'll just use a mock or fetch to see if it generates valid URL
