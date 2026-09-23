6d0e0853b73ca8fa35dac6095f326aa24a14389c
Hoyuuna <nghoanganhtuann@gmail.com>
2026-09-23 19:58:06 +0700

    feat: implement x-api-key protection for api and optimize photo aggregation query

--- src/js/00_core.js
+++ src/js/00_core.js
@@ -358,7 +358,7 @@ Object.assign(window.app, {
                 const qs = searchParams.toString();
                 if (qs) url += "?" + qs;
             }
-            const headers = { "Content-Type": "application/json" };
+            const headers = { "Content-Type": "application/json", "x-api-key": "VNBUS-AUTH-KEY-2026" };
             const token = app.api.getToken();
             if (token) headers["Authorization"] = `Bearer ${token}`;
 
